d5f1897c849d5eea64b2d23c2fc3faf5e1fcac7c
Maazin Sherif <maazinsherif99@gmail.com>
2024-04-16 20:34:54 -0400

    set up express and sequelize

---
+++ backend/config/database.js
@@ -0,0 +1,27 @@
+// Loads the db configuration environment variables from .env
+// Also defines global schema
+const config = require('./index');
+
+module.exports = {
+  development: {
+    storage: config.dbFile,
+    dialect: "sqlite",
+    seederStorage: "sequelize",
+    logQueryParameters: true,
+    typeValidation: true
+  },
+  production: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    seederStorage: 'sequelize',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    },
+    define: {
+      schema: process.env.SCHEMA
+    }
+  }
+};
