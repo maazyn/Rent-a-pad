382edab74777d0b099d89c164b26dffa53f2abec
Maazin Sherif <maazinsherif99@gmail.com>
2024-04-25 17:55:32 -0400

    Update migrations to fix deployment error

--- backend/db/migrations/20240424221622-add-firstName-lastName-to-user.js
+++ backend/db/migrations/20240424221622-add-firstName-lastName-to-user.js
@@ -1,18 +1,23 @@
 'use strict';
-
 /** @type {import('sequelize-cli').Migration} */
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // use the correct schema
+}
+
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    await queryInterface.addColumn('Users', 'firstName', {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.addColumn({ tableName: 'Users', schema: options.schema }, 'firstName', {
       type: Sequelize.STRING,
     });
-    await queryInterface.addColumn('Users', 'lastName', {
+    await queryInterface.addColumn({ tableName: 'Users', schema: options.schema }, 'lastName', {
       type: Sequelize.STRING,
     });
   },
 
-  async down (queryInterface, Sequelize) {
-    await queryInterface.removeColumn('Users', 'firstName');
-    await queryInterface.removeColumn('Users', 'lastName');
+  async down(queryInterface, Sequelize) {
+    await queryInterface.removeColumn({ tableName: 'Users', schema: options.schema }, 'firstName');
+    await queryInterface.removeColumn({ tableName: 'Users', schema: options.schema }, 'lastName');
   },
 };
