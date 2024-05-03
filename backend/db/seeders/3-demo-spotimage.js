94f9b17a849f33ecb49ec4b3e59063f5b7f006dc
Maazin Sherif <maazinsherif99@gmail.com>
2024-05-02 22:18:42 -0400

    Attempt 3

--- backend/db/seeders/3-demo-spotimage.js
+++ backend/db/seeders/3-demo-spotimage.js
@@ -12,12 +12,12 @@ module.exports = {
     await SpotImage.bulkCreate([
       {
         spotId: 1,
-        url: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.mansionglobal.com%2Farticles%2Fa-high-rise-miami-apartment-with-endless-ocean-views-129450&psig=AOvVaw1-nwyqh8CzBDf67vV73chE&ust=1714333286976000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCKiSo7mT44UDFQAAAAAdAAAAABAJ",
+        url: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.mansionglobal.com%2Farticles%2Fa-high-rise-miami-apartment-with-endless-ocean-views-129450&psig=AOvVaw1-nwyqh8CzBDf67vV73chE&ust=1714333286976000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCKi",
         preview: true,
       },
       {
         spotId: 1,
-        url: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.thepinnaclelist.com%2Fdesign%2Fecho-brickell-apartment-interior-design-miami-fl-usa-blanca-wall%2Fattachment%2F006-echo-brickell-apartment-interior-design-miami-fl-usa-blanca-wall%2F&psig=AOvVaw04TrlNp-ol4p0FD28621BP&ust=1714333457717000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCOCZg_2T44UDFQAAAAAdAAAAABBs',
+        url: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.thepinnaclelist.',
         preview: false,
       },
       {
@@ -38,8 +38,8 @@ module.exports = {
     options.tableName = "SpotImages";
     const Op = Sequelize.Op;
     return queryInterface.bulkDelete(options, {
-      url: { [Op.in]: ["https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.mansionglobal.com%2Farticles%2Fa-high-rise-miami-apartment-with-endless-ocean-views-129450&psig=AOvVaw1-nwyqh8CzBDf67vV73chE&ust=1714333286976000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCKiSo7mT44UDFQAAAAAdAAAAABAJ",
-        "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.thepinnaclelist.com%2Fdesign%2Fecho-brickell-apartment-interior-design-miami-fl-usa-blanca-wall%2Fattachment%2F006-echo-brickell-apartment-interior-design-miami-fl-usa-blanca-wall%2F&psig=AOvVaw04TrlNp-ol4p0FD28621BP&ust=1714333457717000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCOCZg_2T44UDFQAAAAAdAAAAABBs",
+      url: { [Op.in]: ["https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.mansionglobal.com%2Farticles%2Fa-high-rise-miami-apartment-with-endless-ocean-views-129450&psig=AOvVaw1-nwyqh8CzBDf67vV73chE&ust=1714333286976000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCKi",
+        "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.thepinnaclelist.",
         "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.mountainliving.com%2Fa-colorado-getaway%2F&psig=AOvVaw3WUwx_vDmmCOzjK9sxgi_h&ust=1714333534209000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCMCo5J2U44UDFQAAAAAdAAAAABAJ",
         "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.thespruce.com%2Flog-cabin-interiors-7564421&psig=AOvVaw1_6CyrxzVMkUMGLZceoIEr&ust=1714333571117000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCNCfyLCU44UDFQAAAAAdAAAAABAO"]},
     }, {})
--- backend/db/seeders/5-demo-reviewimage.js
+++ backend/db/seeders/5-demo-reviewimage.js
@@ -17,7 +17,7 @@ module.exports = {
         },
         {
           reviewId: 3,
-          url: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fnaturalhistory.si.edu%2Feducation%2Fteaching-resources%2Fearth-science%2Fwhen-volcanoes-erupt&psig=AOvVaw3xkJTU_3aO8uVjn4a4dx20&ust=1714333805988000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCPjT7KOV44UDFQAAAAAdAAAAABAE",
+          url: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fnaturalhistory.si.edu%2Feducation%2Fteaching-resources%2Fearth-science%2Fwhen-volcanoes-erupt&psig=AOvVaw3xkJTU_3aO8uVjn4a4dx20&ust=1714333805988000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCPj",
         },
 
     ], { validate: true })
@@ -29,7 +29,7 @@ module.exports = {
     return queryInterface.bulkDelete(options, {
       url: { [Op.in]: [
         "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.businessinsider.com%2Funderrated-things-to-do-in-miami-2018-5&psig=AOvVaw1mEe4xWEyqvOb0t-RvUT69&ust=1714333690454000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCJCRhOuU44UDFQAAAAAdAAAAABAJ",
-        "https://www.google.com/url?sa=i&url=https%3A%2F%2Fnaturalhistory.si.edu%2Feducation%2Fteaching-resources%2Fearth-science%2Fwhen-volcanoes-erupt&psig=AOvVaw3xkJTU_3aO8uVjn4a4dx20&ust=1714333805988000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCPjT7KOV44UDFQAAAAAdAAAAABAE"
+        "https://www.google.com/url?sa=i&url=https%3A%2F%2Fnaturalhistory.si.edu%2Feducation%2Fteaching-resources%2Fearth-science%2Fwhen-volcanoes-erupt&psig=AOvVaw3xkJTU_3aO8uVjn4a4dx20&ust=1714333805988000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCPj"
       ]},
     }, {})
   }
