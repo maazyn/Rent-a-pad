55c7054816c407ee5f33ec4373913642b4b5c9db
Maazin Sherif <maazinsherif99@gmail.com>
2024-06-03 16:27:18 -0400

    Profile dropdown menu

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -20,7 +20,8 @@ const removeUser = () => ({
 
 
 //*THUNKS
-export const login = ({credential, password}) =>  async (dispatch) => {
+export const login = (user) =>  async (dispatch) => {
+    const { credential, password } = user;
     const response = await csrfFetch('/api/session', {
       method: "POST",
       body: JSON.stringify({
