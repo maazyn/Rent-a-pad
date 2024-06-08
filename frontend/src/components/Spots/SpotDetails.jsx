6c8fdb063e5724cb759af6bcee9385502c2ca3de
Maazin Sherif <maazinsherif99@gmail.com>
2024-06-08 15:42:57 -0400

    Add navlink to reserve button and improve styling to spot detail page

--- frontend/src/components/Spots/SpotDetails.jsx
+++ frontend/src/components/Spots/SpotDetails.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 // import * as sessionActions from '../../store/session';
 import { useDispatch, useSelector } from 'react-redux';
+import { NavLink } from 'react-router-dom';
 import { getSpot } from '../../store/spots';
 import { useParams } from 'react-router-dom';
 import { FaStar } from "react-icons/fa6";
@@ -28,11 +29,23 @@ const SpotDetails = () => {
                 <h2>{theSpot.name}</h2>
                 <p>{theSpot.city}, {theSpot.state}, {theSpot.country}</p>
             </div>
-            <div id="details-images">
-                {theSpot.SpotImages.map((image, id) => {
-                <img key={id} src={image.url} />
+            <li id="details-images">
+                <ul id="details-default-main-image">
+                {theSpot.SpotImages.map((image, i) => {
+                    if (image.preview === true) {
+                        {console.log(image.url)}
+                        <img key={i} src={image.url} />
+                    } else return null
                 })}
-            </div>
+                </ul>
+                <ul id="details-rest-images">
+                    {theSpot.SpotImages.map((image, id) => {
+                        if (image.preview === false) {
+                            <img key={id} src={image.url} />
+                        }
+                    })}
+                </ul>
+            </li>
             <div className="details">
                 <div id="details-left">
                     <p>Hosted by {theSpot.ownerId}</p>
@@ -42,7 +55,9 @@ const SpotDetails = () => {
                     <p id="details-right-price">${theSpot.price} night</p>
                     <p id="details-right-rating"><FaStar/> {theSpot.avgStarRating}</p>
                     <p id="details-right-review-number">{theSpot.numReviews} reviews</p>
-                    <button id="details-right-reserve-button">Reserve</button>
+                    {/* <button id="details-right-reserve-button">Reserve</button> */}
+                    <NavLink to={`spots/:${theSpot.id}/bookings`} id="details-right-reserve-button" >Reserve</NavLink>
+
                 </div>
             </div>
                 <div id="details-lower">
@@ -50,8 +65,8 @@ const SpotDetails = () => {
                         <FaStar/> {theSpot.avgStarRating} | {theSpot.numReviews} reviews
                     </h2>
                     <div className="review-card">
-                        {/* {theSpot.SpotImages.map((review, id) => {
-                        <img key={id} src={review.message} />
+                        {/* {theSpot.map((review) => {
+                        <ReviewCard key={spot.id} {...review} />
                         })} */}
                     </div>
                 </div>
